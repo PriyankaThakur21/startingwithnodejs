05d5ee94367db8bf842ddfb9dfecf5a8d1ed32e3
PriyankaThakur21 <thakur.pri112@gmail.com>
2023-05-16 17:11:38 +0530

    arrow Function & object Creation

--- first-app.js
+++ first-app.js
@@ -1,1 +1,11 @@
-console.log('hello world');
+// Write an arrow function which returns the product of two numbers
+const product=(a,b)=>a*b;
+console.log(product(2,3));
+
+// create a student object
+const student={
+    name:'Priyanka',
+    rollNo:21,
+    age:22
+}
+console.log(student);

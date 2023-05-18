98e481ee6ae7b13f61bc0f60bcaa123d8d59b1c8
PriyankaThakur21 <thakur.pri112@gmail.com>
2023-05-18 17:53:48 +0530

    read messages

--- first-app.js
+++ first-app.js
@@ -1,26 +1,39 @@
 const http=require('http');
+const fs=require('fs');
 const server=http.createServer((req,res)=>{
     const url=req.url;
-    if(url==='/home'){
+    const method=req.method;
+    if(url==='/'){
+        fs.readFile('text.txt',{encoding:"utf-8"}, (err,data)=>{
+            if(err){
+                console.log(err);
+            }
+        console.log('data from file='+data);
         res.write('<html>');
-        res.write('<head><title>Home</title></head>');
-        res.write('<body><h1>Welcome Home</h1></body>');
+        res.write('<head><title>Write Message</title></head>');
+        res.write(`<body>${data}</body>`);
+        res.write('<body><form action="/msg" method="POST"><input type="text" name="message"><button>submit</button></form></body>');
         res.write('</html>');
-        res.end();
+        return res.end();
+        })
     }
-    if(url==='/about'){
-        res.write('<html>');
-        res.write('<head><title>About</title></head>');
-        res.write('<body><h1>Welcome to About us page</h1></body>');
-        res.write('</html>');
-        res.end();
-    }
-    if(url==='/node'){
-        res.write('<html>');
-        res.write('<head><title>Main</title></head>');
-        res.write('<body><h1>Welcome to my Node Js project</h1></body>');
-        res.write('</html>');
-        res.end();
-    }
+        if(url==='/msg' && method==='POST'){
+            const body=[];
+            req.on('data',(chunk)=>{
+                body.push(chunk);
+            })
+            req.on('end',()=>{
+                const parsebody=Buffer.concat(body).toString();
+                const message=parsebody.split('=')[1];
+                fs.writeFile('text.txt',message,(err)=>{
+                    if(err){
+                        console.log(err);
+                    }
+                    res.statusCode=302;
+                    res.setHeader('Location', '/');
+                    return res.end();
+                });
+            })
+        }
 })
-server.listen(4000);
+server.listen(7076);

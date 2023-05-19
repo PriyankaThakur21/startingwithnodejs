2bffd6298dea75275135a5fe4c67c4b108110e01
PriyankaThakur21 <thakur.pri112@gmail.com>
2023-05-19 21:51:20 +0530

    parsing incoming request

--- first-app.js
+++ first-app.js
@@ -1,10 +1,21 @@
-const express = require('express')
+const express = require('express');
+const bodyParser=require('body-parser');
+
 const app=express();
-app.use((req, res, next)=>{
-    console.log('first middleware');
+app.use(bodyParser.urlencoded({extended:false}));//urlencoded() is a inbuilt middleware function
+
+app.use('/',(req,res,next)=>{
+    res.send('<h1>Welcome</h1>');
     next();
-});
-app.use((req,res,next)=>{
-    res.send({key1:2});
-});
-app.listen(3000);
+})
+
+app.use('/add-product',(req, res, next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name="name"><button>Add Product</button></form>');
+})
+
+app.post('/product',(req, res, next)=>{
+    console.log(req.body);
+    res.redirect('/');
+})
+
+app.listen(9090);

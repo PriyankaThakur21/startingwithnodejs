e6a412d06aafc6154e893e106e174b08b98c958b
PriyankaThakur21 <thakur.pri112@gmail.com>
2023-05-19 15:02:55 +0530

    starting with express

--- first-app.js
+++ first-app.js
@@ -1,4 +1,10 @@
-const http = require('http');
-const routes = require('./routes');
-const server=http.createServer(routes);
-server.listen(5056);
+const express = require('express')
+const app=express();
+app.use((req, res, next)=>{
+    console.log('first middleware');
+    next();
+});
+app.use((req,res,next)=>{
+    res.send({key1:2});
+});
+app.listen(3000);

c17a21dc697305794799707ee6341d3a2124aff6
PriyankaThakur21 <thakur.pri112@gmail.com>
2023-05-21 14:38:36 +0530

    html and rootDir

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
+const rootDir = require('../util/path');
 
 router.get('/add-product',(req, res, next)=>{
-    res.send('<form action="/product" method="POST"><label>Name</label><input type="text" name="name"><br><label>Size</label><input type="text" name="size"><br><button>Add Product</button></form>');
+    res.sendFile(path.join(rootDir, 'views', 'admin.html'));
 })
 
 router.post('/product',(req, res, next)=>{

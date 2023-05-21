39324dcd90458db1e36152ff3084ca5f528c0b46
PriyankaThakur21 <thakur.pri112@gmail.com>
2023-05-21 20:56:37 +0530

    saving data to file

--- models/products.js
+++ models/products.js
@@ -1,11 +1,24 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
+const rootDir = require('../util/path');
+const { json } = require('body-parser');
 
 module.exports = class Product{
     constructor(name){
         this.name=name;
     }
     save(){
-        products.push(this);
+        const p = path.join(rootDir, 'data', 'products.json');
+        fs.readFile(p, (err, data)=>{
+            let products=[];
+            if(!err){
+                products=JSON.parse(data);
+            }
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err)=>{
+                console.log(err);
+            });
+        })
     }
     static fetchAll(){
         return products;

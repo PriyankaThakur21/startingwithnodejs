928ee177898443f10ded17be4ffc4a4a31ae3f51
PriyankaThakur21 <thakur.pri112@gmail.com>
2023-05-27 16:28:48 +0530

    association between user and product

--- first-app.js
+++ first-app.js
@@ -12,6 +12,9 @@ const contactRoutes = require('./routes/contact');
 
 const errorController = require('./controllers/errors');
 
+const productModel = require('./models/products');
+const userModel =  require('./models/users');
+
 app.use(bodyParser.urlencoded({extended:true}));//urlencoded() is a inbuilt middleware function
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -23,7 +26,10 @@ app.use(contactRoutes);
 
 app.use(errorController.get404);
 
-sequelize.sync()
+productModel.belongsTo(userModel, {constraints: true, onDelete: 'CASCADE'});
+userModel.hasMany(productModel);
+
+sequelize.sync({force:true}) //make new table and deletes previous ones
 .then((res)=>{
     console.log(res);
     app.listen(3000);
